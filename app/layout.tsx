05da00252fed8fa5b37c49f5f26d9d29388cc254
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-10-15 01:02:34 -0300

    feat: added dark and light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +20,17 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={cn("min-h-screen pt-12 antialiased", inter.className)}>
-        <Navbar />
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import { FC } from "react";
 import { buttonVariants } from "./ui/button";
+import { ToggleTheme } from "./ToggleTheme";
 
 const Navbar: FC = () => {
   return (
     <header className="fixed top-0 inset-x-0 h-fit border-b z-[10] py-3">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-        <Link href="/">Courses review</Link>
+        <Link href="/" className="font-bold">
+          MF-review
+        </Link>
 
         <p>search bar here</p>
 
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        <div className="flex items-center gap-2">
+          <ToggleTheme />
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        </div>
       </div>
     </header>
   );
